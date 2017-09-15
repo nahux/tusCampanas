f9aff0fed985dd4930ab870e8487cf90cef9612c
Nahuel Aparicio <nahuel.aparicio@softtek.com>
2017-09-15 14:28:32 -0300

    Agregado dashboard, mejorado login

--- js/inicio.js
+++ js/inicio.js
@@ -6,6 +6,12 @@ app.controller('inicioCtrl', function($scope) {
 
 });
 
+app.controller('loginCtrl', function($scope) {
+	$scope.validateLogin = function(user) {
+		//Validar login
+	}
+});
+
 
 //Componentes
 angular.module('inicio').component('compAbout', {
@@ -21,20 +27,66 @@ angular.module('inicio').component('compAbout', {
 
 //Config States
 app.config(function($stateProvider){
-	//Estado inicio
+	//Estados inivcio
 	var inicioState = {
 		name: 'inicio',
 		url: '/inicio',
 		template: '<h3> Inicio </h3>'
 	}
-	//Estado about
+
 	var aboutState = {
 		name: 'about',
 		url: '/about',
 		component: 'compAbout'
 	}
-
 	//Registro los states
 	$stateProvider.state(inicioState);
 	$stateProvider.state(aboutState);
+	////////////////////
+	//Estados dashboard
+	var campanasState = {
+		name: 'campanas',
+		url: '/campanas',
+		template: '	<div class="input-group"> ' +
+						'<h1>Tus Campañas</h1>' +
+						'<div class="input-group">'+
+						'	<input type="text" class="form-control" placeholder="Campañas...">'+
+						'	<span class="input-group-btn">'+
+						'		<button class="btn btn-default" type="button">Buscar!</button>'+
+						'	</span>' +
+						'</div><br>'+
+						'<div class="list-group"> ' +
+						'	<button type="button" class="list-group-item"> México 3 días</button> ' +
+						'	<button type="button" class="list-group-item"> Córdiba 1 semana</button> ' +
+						'	<button type="button" class="list-group-item"> Crucero Miami</button> ' +
+						'</div>' +
+						'<button class="btn btn-default" type="button">Nueva</button>  '+
+					'</div>'
+		/*templateUrl: 'partial-campanas.html'*/
+	}
+
+	var gruposState = {
+		name: 'grupos',
+		url: '/grupos',
+		template:   '<div class="input-group"> ' +
+						'<h1>Tus Grupos</h1>' +
+						'<div class="input-group">'+
+						'	<input type="text" class="form-control" placeholder="Grupos...">'+
+						'	<span class="input-group-btn">'+
+						'		<button class="btn btn-default" type="button">Buscar!</button>'+ 
+						'	</span>'+
+						'</div><br>'+
+						'<div class="list-group"> ' +
+						'	<button type="button" class="list-group-item">Adultos La Plata</button> ' +
+						'	<button type="button" class="list-group-item">Colegas</button> ' +
+						'	<button type="button" class="list-group-item">Padres</button> ' +
+						'</div>' +
+					'</div>'
+		/*templateUrl: 'partial-grupos.html'*/
+	}
+	
+	//Registro los states
+	$stateProvider.state(campanasState);
+	$stateProvider.state(gruposState);
+	////////////////////
 });
