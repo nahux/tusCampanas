2e5f0e1f0697bb6cd68402ae82dc46a4c32f51ef
Nahu Apa <nahuel94@gmail.com>
2017-11-02 20:29:29 -0300

    Cambios de id para test

---
+++ backend/cypress/integration/test_login.js
@@ -0,0 +1,39 @@
+describe('Logeando', function() {
+	before(function() {
+		Cypress.config('baseUrl','http://localhost:3000/#!/')
+	})
+
+	context('NoAutorizado', function(){
+		it('Se ingresa a campañas sin estar autorizado, redirecciona a login',function(){
+			cy.visit('/dashboard/campanas')
+			cy.url().should('include','/login');
+		})
+	})
+
+	context('Autorizado', function(){
+		beforeEach(function(){
+			cy.visit('/login')
+		})
+
+		it('Redirecciona a /dashboard/campanas al loguear', function(){
+			cy.get('input[name=name]').type('fakeusername')
+			cy.get('input[name=pass]').type('fakepassword')
+			cy.get('#login').click({force:true})
+			cy.url().should('include', '/login')
+			
+		})	
+
+		it('Redirecciona a /dashboard/campanas al loguear', function(){
+			cy.get('input[name=name]').type('nahuapas')
+			cy.get('input[name=pass]').type('12345678')
+			cy.get('#login').click({force:true})
+
+			cy.url().should('include', '/dashboard/campanas')
+
+			cy.get('#grupos').click()
+			cy.wait(200)
+			cy.get('#grupo0').should('have.value', 'Adultos')
+			//cy.get('#grupo1').should('include', 'Adultos La Plata')
+		})
+	})
+})
